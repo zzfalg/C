1ffdbd1dbff87289f69dacd6b2aa805b86a7ec34
zzfalg <160737286+zzfalg@users.noreply.github.com>
2024-02-27 09:38:23 +0800

    Add files via upload

---
+++ 分支.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int age = 120;
+//	if (age < 18)
+//		printf("少年\n");
+//	else if (age >= 18 && age < 30)
+//		printf("青年\n");
+//	else if (age >= 30 && age < 90)
+//		printf("社会人\n");
+//	else
+//		printf("长生者\n");
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 1;
+//	for (i; i <= 101; i++)	
+//	{
+//		if (i % 2 == 1)
+//			printf("%d ", i);
+//	}
+//	return 0;
+//}
