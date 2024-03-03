1f57a8f87606eb8cd41017359d192425c5ba811a
zzfalg <160737286+zzfalg@users.noreply.github.com>
2024-03-03 09:36:45 +0800

    Add files via upload

---
+++ 练手.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	for (i = 1; i <= 10; i++)
+	{
+		if (i == 5)
+			continue;
+		printf("%d ", i);
+	}
+	return 0;
+}
