c5983aa65431dc2697619d650679fe3ef890ed45
zzfalg <160737286+zzfalg@users.noreply.github.com>
2024-04-06 08:54:37 +0800

    Add files via upload

---
+++ 在学习.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	char arr1[] = "welcome to bit!!!!!!";
+	char arr2[] = "####################";
+	int left = 0;
+	int right = strlen(arr1) - 1;
+	while (left<=right)
+	{
+		arr2[left] = arr1[left];
+		arr2[right] = arr1[right];
+		printf("%s\n", arr2);
+		left++;
+		right--;
+	}
+	return 0;
+}
